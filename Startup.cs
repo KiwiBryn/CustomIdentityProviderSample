796e254ef65fad1d98812d05945ebaa1ac5da943
Bryn Howell Lewis <bryn.lewis@devmobile.co.nz>
2023-08-04 09:00:08 +1200

    Got rid of SqlClient version, BrowserLink, EFCore error page and logging whines

--- Startup.cs
+++ Startup.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
+//using Microsoft.EntityFrameworkCore; BHL
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -61,14 +61,14 @@ namespace CustomIdentityProviderSample
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-            loggerFactory.AddDebug();
+            // loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            // loggerFactory.AddDebug();
 
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseDatabaseErrorPage();
-                app.UseBrowserLink();
+                // app.UseDatabaseErrorPage(); BHL
+                // app.UseBrowserLink(); BHL
             }
             else
             {
