31bf26ee126affee6e264a811d0444e26a943569
Bryn Howell Lewis <bryn.lewis@devmobile.co.nz>
2023-08-04 16:36:41 +1200

    Renamed columns and changed type of id

--- CustomProvider/ApplicationUser.cs
+++ CustomProvider/ApplicationUser.cs
@@ -5,11 +5,13 @@ namespace CustomIdentityProviderSample.CustomProvider
 {
     public class ApplicationUser : IIdentity
     {
-        public virtual Guid Id { get; set; } = Guid.NewGuid();
+        // public virtual Guid Id { get; set; } = Guid.NewGuid(); BHL
+        public virtual string Id { get; set; } = Guid.NewGuid().ToString();
         public virtual string UserName { get; set; }
         public virtual string Email { get; set; }
         public virtual bool EmailConfirmed { get; set; }
-        public virtual string TelephoneNumber { get; set; }
+        // public virtual string TelephoneNumber { get; set; } BHL
+        public virtual string PhoneNumber { get; set; }
         public virtual bool TwoFactorEnabled { get; set; }
         public virtual String PasswordHash { get; set; }
         public string NormalizedUserName { get; internal set; }
