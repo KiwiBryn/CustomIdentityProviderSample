18d5b5c457a0b685ec047e81ace601b03aa7968e
Bryn Howell Lewis <bryn.lewis@devmobile.co.nz>
2023-08-04 12:29:52 +1200

    Fixed up namespsace and associated AuthenticalDescription

--- Models/ManageViewModels/ManageLoginsViewModel.cs
+++ Models/ManageViewModels/ManageLoginsViewModel.cs
@@ -2,15 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http.Authentication;
+// using Microsoft.AspNetCore.Http.Authentication; BHL
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Identity;
 
 namespace CustomIdentityProviderSample.Models.ManageViewModels
 {
     public class ManageLoginsViewModel
     {
+        //public IList<AuthenticationDescription> OtherLogins { get; set; } BHL
         public IList<UserLoginInfo> CurrentLogins { get; set; }
 
-        public IList<AuthenticationDescription> OtherLogins { get; set; }
+        // public IList<AuthenticationDescription> OtherLogins { get; set; } BHL
+        public IList<AuthenticationScheme> OtherLogins { get; set; }
     }
 }
